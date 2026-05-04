84d913036b6addb8ca518314c7902a9a498a927f
Satyanshgaur <satyanshgaur0@gmail.com>
2026-05-04 23:54:23 +0530

    imprved mathamatics used for calculating elevation angles across different latitudes of the earth

--- geometry.py
+++ geometry.py
@@ -4,11 +4,10 @@ def slant_range(altitude_km, elevation_deg):
     Re = 6371.0  # Earth radius (km)
     el = math.radians(elevation_deg)
     h = altitude_km
-
     return math.sqrt(
         (Re + h)**2 - (Re * math.cos(el))**2
     ) - Re * math.sin(el)
-
+#Calculating slant range angles for different sattelites 
 
 def effective_elevation(global_elev, latitude):
     # stations far from equator see lower effective elevation
